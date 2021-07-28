3e585be64b7b34da498c9af725ff49d298a6b3e3
kang <1115610574@qq.com>
2021-07-28 11:26:10 +0800

    add first test

---
+++ src/test/java/org/kang/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.kang.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author kang
+ * @date 2021/7/28 11:22
+ */
+class MainTest {
+    @Test
+    public void testBooleanTrue() {
+        assertTrue(Boolean.TRUE);
+    }
+}
